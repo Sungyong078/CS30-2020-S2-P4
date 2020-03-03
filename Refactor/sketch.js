27b9051efc1316079fea190c0b0f1c5e9c9f3a99
Sungyong078 <60625484+Sungyong078@users.noreply.github.com>
2020-03-03 14:10:35 -0600

    refactoring

---
+++ Refactor/sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+function draw() {
+  background(255); stroke(0); line(240, 0, 240, 270); line(0, 135, 480, 135);
+  noStroke(); fill(0);
+  if (mouseX < 240 && mouseY < 135) {
+    rect(0, 0, 240, 135);
+  }
+  else if (mouseX > 240 && mouseY < 135) {
+    rect(240, 0, 240, 135);
+  }
+  else if (mouseX < 240 && mouseY > 135) {
+    rect(0, 135, 240, 135);
+  }
+  else if (mouseX > 240 && mouseY > 135) {
+    rect(240, 135, 240, 135);
+  }
+}
