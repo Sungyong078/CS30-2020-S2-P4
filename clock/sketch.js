f5fde890af3338d8962257fa9ee411e8ab6493e9
Sungyong078 <60625484+Sungyong078@users.noreply.github.com>
2020-03-03 13:36:52 -0600

    clock finished

--- clock/sketch.js
+++ clock/sketch.js
@@ -15,6 +15,33 @@ function draw() {
   push();
   translate(width/2, height/2);
   ellipseMode(CENTER);
-  ellipse(0,0,width*0.3,width*0.3);
+  strokeWeight(4);
+  ellipse(0,0,width*0.4,width*0.4);
+  for(let i = 0; i < 60; i++){
+    if(i%5===0){
+      strokeWeight(4);
+      line(width*0.15,0,width*0.19,0);
+    }
+    else{
+      strokeWeight(2);
+      line(width*0.16,0,width*0.19,0);
+    }
+    rotate(radians(6));
+  }
+  rotate(radians(-90));
+  push();   //new c.s just for sec
+  stroke(255,0,0);
+  strokeWeight(3);
+  rotate(radians(second()*6));
+  line(0,0,width*0.19,0);
+  pop();   //pop seconds c.s
+
+
+  push(); //push min c.s
+  stroke(0,255,0);
+  strokeWeight(3);
+  rotate(radians(minute()*6 + second()/10));
+  line(0,0,width*0.18,0);
+
   pop();
 }
