b95ba75d5f3e67883d8062fcdeafbed7a0c898d6
Sungyong078 <60625484+Sungyong078@users.noreply.github.com>
2020-03-18 13:09:39 -0600

    multi grid final

--- Multi-Coloured Grid/sketch.js
+++ Multi-Coloured Grid/sketch.js
@@ -1,10 +1,10 @@
 // Multi-Coloured Grid
 // Sungyong P
 // 2/27/2020
+//draw random squares with random colors
 
 
-
-let gridSpacing = 30;
+let gridSpacing = 30; //size of square
 
 
 
@@ -15,7 +15,7 @@ function setup() {
 
 
 
-function rectGrid() {
+function rectGrid() { //set color and size of the square
   for (let y = 0; y < height; y += gridSpacing) {
     for (let x = 0; x < width; x += gridSpacing) {
       let ranRed = random(255);
@@ -29,14 +29,19 @@ function rectGrid() {
 
 
 function keyPressed() {
-  if (keyCode === LEFT_ARROW) {
+  if (keyCode === LEFT_ARROW) { //if left arrow is pressed change the color of square
+    background(100);
+    rectGrid();
+  }
+  else if (keyCode === RIGHT_ARROW){ // if right arrow is pressed make the size smaller by 5
+    gridSpacing -= 5;
     background(100);
     rectGrid();
   }
 }
 
 
-function mousePressed() {
+function mousePressed() { //if left button is cliked make the size bigger by 5
   if (mouseIsPressed) {
     if (mouseButton === LEFT) {
       gridSpacing += 5;
