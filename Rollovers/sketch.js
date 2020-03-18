10fc6008bc85e6294304df262a134a89cb0c618a
Sungyong078 <60625484+Sungyong078@users.noreply.github.com>
2020-03-18 12:39:37 -0600

    rollover

--- Rollovers/sketch.js
+++ Rollovers/sketch.js
@@ -2,7 +2,7 @@
 // Sungyong Park
 // Feb 12 2020
 
-let onLeft, onRight,downLeft,downRight; //Boolean State variables
+
 let leftFade = 0;
 let rightFade = 0;
 let downRightFade = 0;
@@ -11,86 +11,35 @@ const FADE_SPEED = 5;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  onLeft = true;
-  onRight = false;
-  downLeft = true;
-  downRight = false;
+
 }
 
 function updateCurrentSide(){
-
-  if(mouseX < width/2 && mouseY < height/2){
-    //mouse is on the LEFT
-    onLeft = false;
-    onRight = true;
-    downLeft = false;
-    downRight = false;
+  if (mouseX < width/2 && mouseY < height/2) { //bottom left
+    rect(0, 0, width/2, height/2);
   }
-  else{
-    //mouse is on the right
-    onRight = false;
-    onLeft = false;
-    downLeft = false;
-    downRight = true;
+  
+  else if (mouseX > width/2 && mouseY < height/2) { //bottom right
+    rect(width/2, 0, width/2, height/2);
   }
-
-  if(mouseX < width/2 && mouseY > height/2){
-    downLeft = false;
-    downRight = true;
-    onLeft = false;
-    onRight = false;
-   
+  
+  else if (mouseX < width/2 && mouseY > height/2) { //top right
+    rect(0, height/2, width/2, height/2);
   }
-  else{
-    downRight = false;
-    downLeft = true;
-    onLeft = false;
-    onRight = false;
-    
+  
+  else if (mouseX > width/2 && mouseY > height/2) { //top left
+    rect(width/2, height/2, width/2, height/2);
   }
-
 }
+  
+
+
 
 function renderRectangles(){
- 
-  if(onLeft){  //onLeft === true
-    fill(0, leftFade);
-    leftFade += FADE_SPEED;
+  if(mouseX < width/2 && mouseY < height/2){
+    
   }
-  else {
-    fill(255);
-    leftFade = 0;
-  }
-  rect(0,0,width/2,height/2);
-  if(onRight){
-    fill(0, rightFade);
-    rightFade += FADE_SPEED;
-  }
-  else {
-    fill(255);
-    rightFade = 0;
-  }
-  rect(width/2,0, width/2, height/2);
-
-  if(downLeft){
-    fill(0, downLeftFade);
-    downLeftFade += FADE_SPEED;
-  }
-  else{
-    fill(255);
-    downLeftFade = 0;
-  }
-  rect(width/2,height/2,width/2,height/2);
-
-  if(downRight){
-    fill(0, downRightFade);
-    downRightFade += FADE_SPEED;
-  }
-  else{
-    fill(255);
-    downRightFade = 0;
-  }
-  rect(0,height/2,width/2,height/2);
+  
 
 }
 
@@ -107,3 +56,32 @@ function draw() {
   }
   
 }
+
+
+//function draw() {
+  //setup background
+  //background(255);
+  
+  //draw lines that divide 4 sections 
+  //stroke(0); 
+  //line(width/2, 0, width/2, height); 
+  //line(0, height/2, width, height/2);
+  //noStroke(); 
+  //fill(0);
+  
+  //if (mouseX < width/2 && mouseY < height/2) { //quad 1
+    //rect(0, 0, width/2, height/2);
+  //}
+  
+  //else if (mouseX > width/2 && mouseY < height/2) { //quad 2
+    //rect(width/2, 0, width/2, height/2);
+  //}
+  
+  //else if (mouseX < width/2 && mouseY > height/2) { //quad 3
+    //rect(0, height/2, width/2, height/2);
+  //}
+  
+  //else if (mouseX > width/2 && mouseY > height/2) { //quad 4
+    //rect(width/2, height/2, width/2, height/2);
+  //}
+//}
